80977cf1cd244ea9017a2dd8d77d0fa23f691d53
Zeena <zinaford.ottawa@gmail.com>
2025-10-29 16:07:43 -0400

    Added Student class with attributes

---
+++ src/Student.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+/**
+ * Student class represents a student in the 4th Year Project Management System
+ */
+public class Student {
+            // Unique identifier for the student
+    private String studentName; // The student's first and last name
+    private long studentID;   // The student's student ID
+    private String studentEmail;  // The student's email
+
+    private boolean reportSubmitted; //
+
+    private Project project;  // The project that the student applied for
+    private List<String> studentAvailability;  // The student's availability for oral presentations
+
+}
