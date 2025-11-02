a4ec3e7f1b55ef47f498a5f5f1777e428e51c1d5
Sandy <sandybillyo@gmail.com>
2025-11-02 14:36:52 -0500

    checking updates

--- src/main/java/org/example/ProfessorWebController.java
+++ src/main/java/org/example/ProfessorWebController.java
@@ -3,7 +3,7 @@ package org.example;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
+////
 @Controller
 @RequestMapping("/professors")
 public class ProfessorWebController {
