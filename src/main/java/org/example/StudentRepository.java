5753f6626632f8066229eb66c529b61f6f956f5c
Manit-J <manitjawa@gmail.com>
2025-11-02 18:22:24 -0500

    feat(student): add student controller

--- src/main/java/org/example/StudentRepository.java
+++ src/main/java/org/example/StudentRepository.java
@@ -1,10 +1,13 @@
 package org.example;
 
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.ArrayList;
 import java.util.List;
 
-import org.example.Student;
-import org.springframework.data.repository.CrudRepository;
-
+@RepositoryRestResource
 public interface StudentRepository extends CrudRepository<Student, Long> {
-
+    @Override
+    List<Student> findAll();
 }
